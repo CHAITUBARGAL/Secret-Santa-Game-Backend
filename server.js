1249a7dbac8167d5a086b99e3406a68c06bbc3bf
Chetan Bargal <100152637+CHAITUBARGAL@users.noreply.github.com>
2025-02-14 17:03:03 +0530

    Add files via upload

---
+++ server.js
@@ -0,0 +1,17 @@
+import dotenv from 'dotenv';
+dotenv.config();
+
+import express from 'express';
+import cors from 'cors';
+import secretSantaRoutes from './routes/secretSantaRoutes.js';
+
+const app = express();
+app.use(express.json());
+app.use(cors());
+
+app.use('/api/secret-santa', secretSantaRoutes);
+
+const PORT = process.env.PORT || 5000;
+app.listen(PORT, () => {
+  console.log(`Server running on port ${PORT}`);
+});
